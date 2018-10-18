bb8279205e6cfd3929b98d7b5891cf0b1c162997
Lukas Puehringer <lukas.puehringer@nyu.edu>
2018-10-18 16:38:32 +0200

    Add basic VerifyLayoutSignatures to verifylib
    Takes a layout Metablock and map of Key objects which must not be
    empty and tries to verify a signature in the metablock's Signatures
    list for each Key in the map.

--- in_toto/verifylib.go
+++ in_toto/verifylib.go
@@ -1,7 +1,27 @@
 package main
 
+
+
+func VerifyLayoutSignatures(layoutMb Metablock, layoutKeys map[string]Key) {
+  if len(layoutKeys) < 1 {
+    panic("Layout signature verification requires at least one key.")
+  }
+
+  for _, key := range layoutKeys {
+    layoutMb.VerifySignature(key)
+  }
+}
+
+
 func InTotoVerify(layoutPath string, layoutKeys map[string]Key, linkDir string) {
 
+  var layoutMb Metablock
+
+  // Load layout
+  layoutMb.Load(layoutPath)
+
+  // Verify root signatures
+  VerifyLayoutSignatures(layoutMb, layoutKeys)
 }
 
 
