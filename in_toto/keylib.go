ed0d57bb34c80dffb6cbd12274b1667458149202
Lukas Puehringer <lukas.puehringer@nyu.edu>
2018-10-18 16:03:36 +0200

    Add load public key method
    The receiver parses a PEM formatted public rsa key file into a Key
    object.

--- in_toto/keylib.go
+++ in_toto/keylib.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "os"
   "fmt"
   "encoding/pem"
   "encoding/hex"
@@ -11,6 +12,61 @@ import (
   "crypto/sha256"
   "strings"
 )
+
+func (k *Key) LoadPublicKey(path string) {
+  keyFile, _ := os.Open(path)
+  defer keyFile.Close()
+
+  // Read key bytes and decode PEM
+  keyBytes, _ := ioutil.ReadAll(keyFile)
+  data, _ := pem.Decode([]byte(keyBytes))
+
+  // Try if this is a indeed a public key
+  x509.ParsePKIXPublicKey(data.Bytes)
+
+  // Strip leading and trailing data from PEM file like securesystemslib does
+  // TODO: Should we instead use the parsed public key to recontsruct the PEM?
+  keyHeader := "-----BEGIN PUBLIC KEY-----"
+  keyFooter := "-----END PUBLIC KEY-----"
+  keyStart := strings.Index(string(keyBytes), keyHeader)
+  keyEnd := strings.Index(string(keyBytes), keyFooter) + len(keyFooter)
+  keyBytesStripped := keyBytes[keyStart:keyEnd]
+
+  // Declare values for key
+  // FIXME: We sholud not hardcode these
+  keyType := "rsa"
+  scheme := "rsassa-pss-sha256"
+  keyIdHashAlgorithms := []string{"sha256", "sha512"}
+
+  // Create partial key map used to create the keyid
+  // Unfortunately we can't use the Key object because this will also carry
+  // yet unwanted fields, such as KeyId and KeyVal.Private and therefore
+  // produce a different hash
+  var keyToBeHashed = map[string]interface{}{
+    "keytype": keyType,
+    "scheme": scheme,
+    "keyid_hash_algorithms": keyIdHashAlgorithms,
+    "keyval": map[string]string{
+      "public": string(keyBytesStripped),
+    },
+  }
+
+  // Canonicalize key and get hex representation of hash
+  keyCanonical := encode_canonical(keyToBeHashed)
+  keyHashed := sha256.Sum256(keyCanonical)
+
+  // Unmarshalling the canonicalized key into the Key object would seem natural
+  // Unfortunately our mandated canonicalization function produces a bytestream
+  // that cannot be unmarshalled by Golang's json decoder, hence we have to
+  // manually assign the values
+  k.KeyType = keyType
+  k.KeyVal = KeyVal{
+      Public: string(keyBytesStripped),
+    }
+  k.Scheme = scheme
+  k.KeyIdHashAlgorithms = keyIdHashAlgorithms
+  k.KeyId = fmt.Sprintf("%x", keyHashed)
+}
 
 
 func VerifySignature(key Key, sig Signature, data []byte) {
