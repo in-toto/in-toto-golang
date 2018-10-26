c14ef09e6e5ff8929e4f170e2c14ec9542da4b86
Lukas Puehringer <lukas.puehringer@nyu.edu>
2018-10-26 15:21:37 +0200

    Add setup/teardown to tests
    In the setup we create a temporary directory, copy all test files
    (layout, links, pubkey) over and change to that dir. When the
    tests are finished we change back and remove the directory.

--- in_toto/verifylib_test.go
+++ in_toto/verifylib_test.go
@@ -2,13 +2,53 @@ package intoto
 
 import (
   "os"
+  "fmt"
   "testing"
+  "path/filepath"
+  "io/ioutil"
   )
 
+const testData = "../test/data"
+
+// TestMain calls all Test*'s of this package (intoto) explicitly with m.Run
+// This can be used for test setup and teardown, e.g. copy test data to a tmp
+// test dir, change to that dir and remove the and contents in the end
+func TestMain(m *testing.M) {
+  testDir, err := ioutil.TempDir("", "in_toto_test_dir")
+  if err != nil {
+    panic("Cannot create temp test dir")
+  }
+
+  // Copy test files to temp test directory
+  // NOTE: Only works for a flat directory of files
+  testFiles, _ := filepath.Glob(filepath.Join(testData, "*"))
+  for _, inputPath := range testFiles {
+    input, err := ioutil.ReadFile(inputPath)
+    if err != nil {
+      panic(fmt.Sprintf("Cannot copy test files (read error: %s)", err))
+    }
+    outputPath := filepath.Join(testDir, filepath.Base(inputPath))
+    err = ioutil.WriteFile(outputPath, input, 0644)
+    if err != nil {
+      panic(fmt.Sprintf("Cannot copy test files (write error: %s)", err))
+    }
+  }
+
+  cwd, _:= os.Getwd()
+  os.Chdir(testDir)
+
+  // Always change back to where we were and remove the temp directory
+  defer os.Chdir(cwd)
+  defer os.RemoveAll(testDir)
+
+  // Run tests
+  os.Exit(m.Run())
+}
+
 func TestInTotoVerifyPass(t *testing.T) {
-  layoutPath := "../test/data/demo.layout.template"
-  pubKeyPath := "../test/data/alice.pub"
-  linkDir := "../test/data"
+  layoutPath := "demo.layout.template"
+  pubKeyPath := "alice.pub"
+  linkDir := "."
 
   var pubKey Key
   if err := pubKey.LoadPublicKey(pubKeyPath); err != nil {
