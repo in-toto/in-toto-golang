a2aa5c421b66f0e28141f204b803ca3ebe7dab58
Lukas Puehringer <lukas.puehringer@nyu.edu>
2018-11-14 16:19:31 +0100

    Add common field inheritance for steps/inspections
    Steps and inspections (supply chain items) share some common
    fields that used to be defined redundantly before.
    This commit adds a new struct `SupplyChainItem` that defines the
    common fields, and adds SupplyChainItem as anonymous field to
    the Step and Inspection struct definitions (which is akin to
    inheritance).

--- in_toto/model.go
+++ in_toto/model.go
@@ -38,13 +38,17 @@ type Link struct {
 const LinkNameFormat = "%s.%.8s.link"
 const LinkNameFormatShort = "%s.link"
 
-type Inspection struct {
-  Type string `json:"_type"`
-  Run []string `json:"run"`
-  // TODO: Abstraction for Steps and Inspections?
+
+type SupplyChainItem struct {
   Name string  `json:"name"`
   ExpectedMaterials [][]string `json:"expected_materials"`
   ExpectedProducts [][]string `json:"expected_products"`
+}
+
+type Inspection struct {
+  Type string `json:"_type"`
+  Run []string `json:"run"`
+  SupplyChainItem
 }
 
 type Step struct {
@@ -52,11 +56,7 @@ type Step struct {
   PubKeys []string `json:"pubkeys"`
   ExpectedCommand []string `json:"expected_command"`
   Threshold int `json:"threshold"`
-
-  // TODO: Abstraction for Steps and Inspections?
-  Name string  `json:"name"`
-  ExpectedMaterials [][]string `json:"expected_materials"`
-  ExpectedProducts [][]string `json:"expected_products"`
+  SupplyChainItem
 }
 
 type Layout struct {
