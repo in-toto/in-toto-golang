344b9fbb268df8117c959d07371572807a3d6df9
Lukas Puehringer <lukas.puehringer@nyu.edu>
2019-02-08 11:55:08 +0100

    Refactor (auto-simplify) rulelib tests
    Simplify with `gofmt -s`

--- in_toto/rulelib_test.go
+++ in_toto/rulelib_test.go
@@ -5,42 +5,41 @@ import (
 )
 
 func TestUnpackValidRules(t *testing.T) {
-
 	// A list of valid rules (lists)
 	// Each will be passed to rulelib.UnpackRule below
 	rules := [][]string{
-		[]string{"CREATE", "foo"},
-		[]string{"DELETE", "foo"},
-		[]string{"MODIFY", "foo"},
-		[]string{"ALLOW", "foo"},
-		[]string{"DISALLOW", "foo"},
-		[]string{"MATCH", "foo", "IN", "source-path", "WITH", "PRODUCTS", "IN",
+		{"CREATE", "foo"},
+		{"DELETE", "foo"},
+		{"MODIFY", "foo"},
+		{"ALLOW", "foo"},
+		{"DISALLOW", "foo"},
+		{"MATCH", "foo", "IN", "source-path", "WITH", "PRODUCTS", "IN",
 			"dest-path", "FROM", "step-name"},
-		[]string{"MATCH", "foo", "IN", "source-path", "WITH", "MATERIALS",
+		{"MATCH", "foo", "IN", "source-path", "WITH", "MATERIALS",
 			"FROM", "step-name"},
-		[]string{"MATCH", "foo", "WITH", "PRODUCTS", "IN", "dest-path",
+		{"MATCH", "foo", "WITH", "PRODUCTS", "IN", "dest-path",
 			"FROM", "step-name"},
-		[]string{"MATCH", "foo", "WITH", "MATERIALS", "FROM", "step-name"},
+		{"MATCH", "foo", "WITH", "MATERIALS", "FROM", "step-name"},
 	}
 
 	// These are the expected results from rulelib.UnpackRule for above rules
 	// (associated by index)
 	expectedRuleMaps := []map[string]string{
-		map[string]string{"type": "create", "pattern": "foo"},
-		map[string]string{"type": "delete", "pattern": "foo"},
-		map[string]string{"type": "modify", "pattern": "foo"},
-		map[string]string{"type": "allow", "pattern": "foo"},
-		map[string]string{"type": "disallow", "pattern": "foo"},
-		map[string]string{"type": "match", "pattern": "foo",
+		{"type": "create", "pattern": "foo"},
+		{"type": "delete", "pattern": "foo"},
+		{"type": "modify", "pattern": "foo"},
+		{"type": "allow", "pattern": "foo"},
+		{"type": "disallow", "pattern": "foo"},
+		{"type": "match", "pattern": "foo",
 			"srcPrefix": "source-path", "dstPrefix": "dest-path",
 			"dstType": "products", "dstName": "step-name"},
-		map[string]string{"type": "match", "pattern": "foo",
+		{"type": "match", "pattern": "foo",
 			"srcPrefix": "source-path", "dstPrefix": "",
 			"dstType": "materials", "dstName": "step-name"},
-		map[string]string{"type": "match", "pattern": "foo",
+		{"type": "match", "pattern": "foo",
 			"srcPrefix": "", "dstPrefix": "dest-path",
 			"dstType": "products", "dstName": "step-name"},
-		map[string]string{"type": "match", "pattern": "foo",
+		{"type": "match", "pattern": "foo",
 			"srcPrefix": "", "dstPrefix": "",
 			"dstType": "materials", "dstName": "step-name"},
 	}
@@ -64,17 +63,16 @@ func TestUnpackValidRules(t *testing.T) {
 
 func TestUnpackInvalidRules(t *testing.T) {
 	rules := [][]string{
-		[]string{"CREATE", "foo", "too-long"},
-		[]string{"SUBVERT", "foo"},
-		[]string{"MODIFY"},
-		[]string{"MATCH", "foo", "too-many-patterns", "IN", "source-path", "WITH",
+		{"CREATE", "foo", "too-long"},
+		{"SUBVERT", "foo"},
+		{"MODIFY"},
+		{"MATCH", "foo", "too-many-patterns", "IN", "source-path", "WITH",
 			"PRODUCTS", "IN", "dest-path", "FROM", "step-name"},
-		[]string{"MATCH", "foo", "WITH", "GUMMY", "BEARS"},
+		{"MATCH", "foo", "WITH", "GUMMY", "BEARS"},
 	}
 	for _, rule := range rules {
 		if _, err := UnpackRule(rule); err == nil {
 			t.Errorf("Invalid rule %s should return error from UnpackRule.", rule)
 		}
 	}
-
 }
