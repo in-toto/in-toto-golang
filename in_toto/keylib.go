5bce89f4acad06384b1ac24c891390dfd5108549
Christian Rebischke <chris@shibumi.dev>
2020-08-07 13:56:25 +0200

    enhance documentation
    In this commit we fix various spelling errors,
    deliver more detailed documentation and change the default error
    message of ErrInvalidKeyType.

--- in_toto/keylib.go
+++ in_toto/keylib.go
@@ -30,8 +30,8 @@ var ErrUnsupportedKeyType = errors.New("unsupported key type")
 // ErrInvalidSignature is returned when the signature is invalid
 var ErrInvalidSignature = errors.New("invalid signature")
 
-// ErrInvalidKeyType is returned when the key is valid, but it has a wrong key type attached to it
-var ErrInvalidKeyType = errors.New("valid key, but not matching key type detected")
+// ErrInvalidKeyType is returned when the keytype is invalid for the given key
+var ErrInvalidKeyType = errors.New("invalid key type for this key")
 
 /*
 GenerateKeyId creates a partial key map and generates the key ID
@@ -88,14 +88,13 @@ func GeneratePublicPemBlock(pubKeyBytes []byte) []byte {
 /*
 SetKeyComponents sets all components in our key object.
 Furthermore it makes sure to remove any trailing and leading whitespaces or newlines.
+We treat key types differently for interoperability reasons to the in-toto python
+implementation and the securesystemslib.
 */
 func (k *Key) SetKeyComponents(pubKeyBytes []byte, privateKeyBytes []byte, keyType string, scheme string, keyIdHashAlgorithms []string) error {
 	// assume we have a privateKey if the key size is bigger than 0
 	switch keyType {
 	case "rsa", "ecdsa":
-		// We need to treat RSA differently, because of interoperability
-		// reasons with the securesystemslib and the in-toto python
-		// implementation
 		if len(privateKeyBytes) > 0 {
 			k.KeyVal = KeyVal{
 				Private: strings.TrimSpace(string(privateKeyBytes)),
@@ -130,8 +129,8 @@ func (k *Key) SetKeyComponents(pubKeyBytes []byte, privateKeyBytes []byte, keyTy
 }
 
 /*
-ParseKey tries to parse a PEM []byte slice.
-Supported are:
+ParseKey tries to parse a PEM []byte slice. Using the following standards
+in the given order:
 
 	* PKCS8
 	* PKCS1
@@ -189,13 +188,13 @@ func (k *Key) LoadKey(path string, scheme string, keyIdHashAlgorithms []string) 
 			err = closeErr
 		}
 	}()
-	// Read key bytes and decode PEM
+	// Read key bytes
 	pemBytes, err := ioutil.ReadAll(pemFile)
 	if err != nil {
 		return err
 	}
 
-	// pam.Decode returns the parsed pem block and a rest.
+	// pem.Decode returns the parsed pem block and a rest.
 	// The rest is everything, that could not be parsed as PEM block.
 	// Therefore we can drop this via using the blank identifier "_"
 	data, _ := pem.Decode(pemBytes)
@@ -217,7 +216,7 @@ func (k *Key) LoadKey(path string, scheme string, keyIdHashAlgorithms []string) 
 			return err
 		}
 	case *rsa.PrivateKey:
-		// Note: We store the public key as PKCS8 key here, although the private key get's stored as PKCS1 key
+		// Note: RSA Public Keys will get stored as X.509 SubjectPublicKeyInfo (RFC5280)
 		// This behavior is consistent to the securesystemslib
 		pubKeyBytes, err := x509.MarshalPKIXPublicKey(key.(*rsa.PrivateKey).Public())
 		if err != nil {
@@ -257,11 +256,12 @@ func (k *Key) LoadKey(path string, scheme string, keyIdHashAlgorithms []string) 
 GenerateSignature will automatically detect the key type and sign the signable data
 with the provided key. If everything goes right GenerateSignature will return
 a for the key valid signature and err=nil. If something goes wrong it will
-return an not initialized signature and an error. Possible errors are:
+return a not initialized signature and an error. Possible errors are:
 
 	* ErrNoPEMBlock
 	* ErrUnsupportedKeyType
 
+Currently supported is only one scheme per key.
 */
 func GenerateSignature(signable []byte, key Key) (Signature, error) {
 	var signature Signature
@@ -271,7 +271,7 @@ func GenerateSignature(signable []byte, key Key) (Signature, error) {
 	// in which we are storing RSA keys in PEM format, but ed25519 keys hex encoded.
 	switch key.KeyType {
 	case "rsa", "ecdsa":
-		// pam.Decode returns the parsed pem block and a rest.
+		// pem.Decode returns the parsed pem block and a rest.
 		// The rest is everything, that could not be parsed as PEM block.
 		// Therefore we can drop this via using the blank identifier "_"
 		data, _ := pem.Decode([]byte(key.KeyVal.Private))
@@ -308,7 +308,8 @@ func GenerateSignature(signable []byte, key Key) (Signature, error) {
 				return signature, nil
 			}
 			// Generate the ecdsa signature on the same way, as we do in the securesystemslib
-			// We construct a
+			// We are marshalling the ecdsaSignature struct as ASN.1 INTEGER SEQUENCES
+			// into an ASN.1 Object.
 			signatureBuffer, err = asn1.Marshal(EcdsaSignature{
 				R: r,
 				S: s,
@@ -338,18 +339,21 @@ Supported key types are:
 
 	* RSA
 	* ED25519
+	* ECDSA
 
-When encountering a RSA key, VerifySignature will decode the PEM block in the key
+When encountering an RSA key, VerifySignature will decode the PEM block in the key
 and will call rsa.VerifyPSS() for verifying the RSA signature.
-When encountering an ed25519 key, Verifysignature will decode the hex string encoded
+When encountering an ed25519 key, VerifySignature will decode the hex string encoded
 public key and will use ed25519.Verify() for verifying the ed25519 signature.
+When the given key is an ecdsa key, VerifySignature will unmarshall the ASN1 object
+and will use the retrieved ecdsa components 'r' and 's' for verifying the signature.
 On success it will return nil. In case of an unsupported key type or any other error
 it will return an error.
 */
 func VerifySignature(key Key, sig Signature, unverified []byte) error {
 	switch key.KeyType {
 	case "rsa", "ecdsa":
-		// pam.Decode returns the parsed pem block and a rest.
+		// pem.Decode returns the parsed pem block and a rest.
 		// The rest is everything, that could not be parsed as PEM block.
 		// Therefore we can drop this via using the blank identifier "_"
 		data, _ := pem.Decode([]byte(key.KeyVal.Public))
