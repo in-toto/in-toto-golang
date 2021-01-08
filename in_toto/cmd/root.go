53b83d34b80b74681bf5780139eb6883b8f14006
Cole Kennedy <colek42@gmail.com>
2021-01-08 13:34:04 -0600

    Use Flags

--- in_toto/cmd/root.go
+++ in_toto/cmd/root.go
@@ -6,49 +6,55 @@ import (
 
 	intoto "github.com/boxboat/in-toto-golang/in_toto"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "hugo",
-	Short: "Hugo is a very fast static site generator",
-	Long: `A Fast and Flexible Static Site Generator built with
-				  love by spf13 and friends in Go.
-				  Complete documentation is available at http://hugo.spf13.com`,
+	Use:   "in-toto",
+	Short: "Framework to secure integrity of software supply chains",
+	Long:  `A framework to secure the integrity of software supply chains https://in-toto.io/`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
+		fmt.Println(pubKeyPath)
+		var layoutMb intoto.Metablock
+		if err := layoutMb.Load(layoutPath); err != nil {
+			fmt.Println(err.Error())
+		}
+
+		var pubKey intoto.Key
+		if err := pubKey.LoadKey(pubKeyPath, "rsassa-pss-sha256", []string{"sha256", "sha512"}); err != nil {
+			fmt.Println(err.Error())
+		}
+
+		var layoutKeys = map[string]intoto.Key{
+			pubKey.KeyId: pubKey,
+		}
+
+		result, err := intoto.InTotoVerify(layoutMb, layoutKeys, linkDir, "", make(map[string]string))
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		resultjson, err := result.GetSignableRepresentation()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println(resultjson)
 	},
 }
 
+var layoutPath string
+var pubKeyPath string
+var linkDir string
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&layoutPath, "layout-path", "", "Full Path For Layout")
+	rootCmd.PersistentFlags().StringVar(&pubKeyPath, "pubkey-path", "", "Full Path For Public Key")
+	rootCmd.PersistentFlags().StringVar(&linkDir, "link-metadata", "", "Link metadata directory")
+
+	viper.BindPFlags(rootCmd.PersistentFlags())
+}
+
 func Execute() {
-	layoutPath := "/home/nkennedy/proj/in-toto-golang/test/data/demo.layout"
-	pubKeyPath := "/home/nkennedy/proj/in-toto-golang/test/data/alice.pub"
-	linkDir := "/home/nkennedy/proj/in-toto-golang/test/data/."
-
-	var layoutMb intoto.Metablock
-	if err := layoutMb.Load(layoutPath); err != nil {
-		fmt.Println(err.Error())
-	}
-
-	var pubKey intoto.Key
-	if err := pubKey.LoadKey(pubKeyPath, "rsassa-pss-sha256", []string{"sha256", "sha512"}); err != nil {
-		fmt.Println(err.Error())
-	}
-
-	var layoutKeys = map[string]intoto.Key{
-		pubKey.KeyId: pubKey,
-	}
-
-	result, err := intoto.InTotoVerify(layoutMb, layoutKeys, linkDir, "", make(map[string]string))
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	resultjson, err := result.GetSignableRepresentation()
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	fmt.Println(resultjson)
-
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
