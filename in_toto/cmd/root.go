ef45d5498f0c0f32269f706aa21a52139193f10e
Cole Kennedy <colek42@gmail.com>
2021-01-08 20:16:22 -0600

    initial verify support

--- in_toto/cmd/root.go
+++ in_toto/cmd/root.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 
-	intoto "github.com/boxboat/in-toto-golang/in_toto"
 	"github.com/spf13/cobra"
 )
 
@@ -13,42 +12,30 @@ var rootCmd = &cobra.Command{
 	Short: "Framework to secure integrity of software supply chains",
 	Long:  `A framework to secure the integrity of software supply chains https://in-toto.io/`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(pubKeyPath)
-		var layoutMb intoto.Metablock
-		if err := layoutMb.Load(layoutPath); err != nil {
-			fmt.Println(err.Error())
-		}
 
-		var pubKey intoto.Key
-		if err := pubKey.LoadKey(pubKeyPath, "rsassa-pss-sha256", []string{"sha256", "sha512"}); err != nil {
-			fmt.Println(err.Error())
-		}
-
-		var layoutKeys = map[string]intoto.Key{
-			pubKey.KeyId: pubKey,
-		}
-
-		result, err := intoto.InTotoVerify(layoutMb, layoutKeys, linkDir, "", make(map[string]string))
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-
-		resultjson, err := result.GetSignableRepresentation()
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		fmt.Println(resultjson)
 	},
 }
 
 var layoutPath string
-var pubKeyPath string
+var pubKeyPaths []string
 var linkDir string
 
 func init() {
-	rootCmd.PersistentFlags().StringVar(&layoutPath, "layout-path", "", "Full Path For Layout")
-	rootCmd.PersistentFlags().StringVar(&pubKeyPath, "pubkey-path", "", "Full Path For Public Key")
-	rootCmd.PersistentFlags().StringVar(&linkDir, "link-metadata", "", "Link metadata directory")
+	rootCmd.PersistentFlags().StringVarP(&layoutPath,
+		"layout", "l", "",
+		`Path to root layout specifying the software supply chain to be 
+		verified`)
+	rootCmd.PersistentFlags().StringSliceVar(&pubKeyPaths,
+		"layout-keys", []string{},
+		`Path(s) to PEM formatted public key(s), used to verify the passed 
+	 	root layout's signature(s). Passing at	least one key using
+	 	'--layout-keys' is	required. For each passed key the layout 
+	 	must carry a valid signature.`)
+	rootCmd.PersistentFlags().StringVarP(&linkDir,
+		"link-dir", "d", "",
+		`Path to directory where link metadata files for steps defined in 
+		the root layout should be loaded from. If not passed links are 
+		loaded from the current working	directory.`)
 }
 
 func Execute() {
