de7123d039082f6e874fb9d9e422d7bdfa548b15
Cole Kennedy <colek42@gmail.com>
2021-01-08 20:16:22 -0600

    initial verify support

---
+++ in_toto/cmd/verify.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"fmt"
+
+	intoto "github.com/boxboat/in-toto-golang/in_toto"
+	"github.com/spf13/cobra"
+)
+
+var verifyCmd = &cobra.Command{
+	Use:   "verify",
+	Short: "Verify that the software supply chain of the delivered product",
+	Long: `in-toto-verify is the main verification tool of the suite, and 
+	it is used to verify that the software supply chain of the delivered 
+	product was carried out as defined in the passed in-toto supply chain 
+	layout. Evidence for supply chain steps must be available in the form 
+	of link metadata files named ‘<step name>.<functionary keyid prefix>.link’.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var layoutMb intoto.Metablock
+
+		if err := layoutMb.Load(layoutPath); err != nil {
+			fmt.Println(err.Error())
+		}
+
+		//Load Keys
+		layoutKeys := make(map[string]intoto.Key, len(pubKeyPaths))
+
+		for _, pubKeyPath := range pubKeyPaths {
+			var pubKey intoto.Key
+
+			if err := pubKey.LoadKey(pubKeyPath, "rsassa-pss-sha256", []string{"sha256", "sha512"}); err != nil {
+				fmt.Println(err.Error())
+			}
+
+			layoutKeys[pubKey.KeyId] = pubKey
+		}
+
+		result, err := intoto.InTotoVerify(layoutMb, layoutKeys, linkDir, "", make(map[string]string))
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		resultjson, err := result.GetSignableRepresentation()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println(resultjson)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(verifyCmd)
+}
