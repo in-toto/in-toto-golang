c64f64d5c93524bd7b4780a57bf3c58af3cfe7b9
Luke Hinds <luke@redhat.com>
2021-10-08 13:44:35 +0100

    Configure cobra using standard layout
    This sets cobra up using the standard approach (the default when
    running `cobra init`
    
    It's then easier to run the app with commands such as:
    
    `go run main.go run`
    
    Signed-off-by: Luke Hinds <luke@redhat.com>

---
+++ cmd/verify.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Verify that generated Markdown docs are up-to-date.
+tmpdir=$(mktemp -d)
+go run ./cmd gendoc --dir "$tmpdir"
+echo "###########################################"
+echo "If diffs are found, run: go run ./cmd gendoc"
+echo "###########################################"
+diff -Naur "$tmpdir" doc/
