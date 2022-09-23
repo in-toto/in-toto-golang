d748c2d612c7631ce2191826a1709614c5d56f41
Aditya Sirish <aditya@saky.in>
2022-09-23 17:10:12 -0400

    Ensure registration is complete
    Signed-off-by: Aditya Sirish <aditya@saky.in>

--- test-infra/infra-up.sh
+++ test-infra/infra-up.sh
@@ -5,4 +5,5 @@ docker-compose -f ./test-infra/docker-compose.yaml up -d spire-server
 sleep 5  # spire-server needs to be fully initialized before spire-agent comes up
 docker-compose -f ./test-infra/docker-compose.yaml up -d spire-agent
 sh ./test-infra/register.sh
+sleep 5  # ensures package registration is completed before it is required
 sh ./test-infra/show-bundle.sh
