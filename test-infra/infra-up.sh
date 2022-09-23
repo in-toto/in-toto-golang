4c02999c35bfe7d3bee433f547ef1368e9a38115
Aditya Sirish <aditya@saky.in>
2022-09-23 15:24:16 -0400

    Bring up spire-server and spire-agent separately
    The agent container depends on the server being fully up
    or it'll fail. The depends_on clause in docker-compose
    is not sufficient, so there's an artificial delay enforced
    to ensure spire-server is fully up.
    
    Signed-off-by: Aditya Sirish <aditya@saky.in>

--- test-infra/infra-up.sh
+++ test-infra/infra-up.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 mkdir -p /tmp/spire/sockets
-docker-compose -f ./test-infra/docker-compose.yaml up -d spire-server spire-agent
-sleep 5
+docker-compose -f ./test-infra/docker-compose.yaml up -d spire-server
+sleep 5  # spire-server needs to be fully initialized before spire-agent comes up
 docker-compose -f ./test-infra/docker-compose.yaml up -d spire-agent
 sh ./test-infra/register.sh
 sh ./test-infra/show-bundle.sh
